d38023bf8ce38e0c109c342fc7323f8e8ea019ec
Alex <alexandralaudon@gmail.com>
2015-03-08 14:57:30 -0600

    Completed rspec spec/features/0_user_signup_spec.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+
+  get 'sign-up', to: 'registrations#new'
+  post 'sign-up', to: 'registrations#create'
+
+  resources :cheeses, only: [:index]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
